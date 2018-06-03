d1f1d9288b617823e27411b496b731c87c0e20da
harry.zhang <zjf2671@126.com>
2018-06-03 19:23:12 +0800

    增加404异常拦截

--- templateProject/src/main/java/com/ddc/template/common/exception/WebExceptionHandler.java
+++ templateProject/src/main/java/com/ddc/template/common/exception/WebExceptionHandler.java
@@ -6,13 +6,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.ddc.template.common.utils.R;
 
 /**
  * @Description: 异常处理器
  * @author harry.zhang
- * @CreateDate:	2018年3月25日
+ * @CreateDate: 2018年3月25日
  * @version 1.0
  */
 @RestControllerAdvice
@@ -23,36 +24,42 @@ public class WebExceptionHandler {
 	 * 处理自定义异常
 	 */
 	@ExceptionHandler(BusinessException.class)
-	public R handleBusinessException(BusinessException e){
+	public R handleBusinessException(BusinessException e) {
 		R r = new R();
 		r.put("code", e.getCode());
 		r.put("msg", e.getMessage());
 
 		return r;
 	}
-	
+
 	@ExceptionHandler(SOAException.class)
-	public R soaException(SOAException e){
+	public R soaException(SOAException e) {
 		logger.error(e.getMessage(), e);
 		return R.error();
 	}
 
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public R handlerNoFoundException(Exception e) {
+		logger.error(e.getMessage(), e);
+		return R.error(404, "路径不存在，请检查路径是否正确");
+	}
+
 	@ExceptionHandler(DuplicateKeyException.class)
-	public R handleDuplicateKeyException(DuplicateKeyException e){
+	public R handleDuplicateKeyException(DuplicateKeyException e) {
 		logger.error(e.getMessage(), e);
 		return R.error("数据库中已存在该记录");
 	}
 
 	@ExceptionHandler(AuthorizationException.class)
-	public R handleAuthorizationException(AuthorizationException e){
+	public R handleAuthorizationException(AuthorizationException e) {
 		logger.error(e.getMessage(), e);
 		return R.error("没有权限，请联系管理员授权");
 	}
 
 	@ExceptionHandler(Exception.class)
-	public R handleException(Exception e){
+	public R handleException(Exception e) {
 		logger.error(e.getMessage(), e);
 		return R.error();
 	}
-	
+
 }
