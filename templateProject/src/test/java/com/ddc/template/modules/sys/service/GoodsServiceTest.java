045f85e7b3728e76261a1ea21d50291eb079ac1f
harry.zhang <zjf2671@126.com>
2018-06-16 12:48:59 +0800

    升级plus 为2.3

---
+++ templateProject/src/test/java/com/ddc/template/modules/sys/service/GoodsServiceTest.java
@@ -0,0 +1,28 @@
+package com.ddc.template.modules.sys.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.ddc.template.modules.sys.entity.GoodsEntity;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoodsServiceTest {
+	
+	@Autowired
+	private GoodsService goodsService;
+
+	@Test
+	public void testUpdateForSet() {
+		GoodsEntity goods = new GoodsEntity();
+		goods.setGoodsId(3L);
+		boolean updateForSet = goodsService.updateForSet("name=''", new EntityWrapper<>(goods));
+		Assert.assertTrue(updateForSet);
+	}
+
+}
