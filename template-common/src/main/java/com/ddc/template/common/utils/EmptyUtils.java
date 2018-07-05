e223dc7df633dd73b9004e7e81cc1521015ebd49
harry.zhang <harry.zhang@daydaycook.com>
2018-07-05 14:53:22 +0800

    判空工具类

---
+++ template-common/src/main/java/com/ddc/template/common/utils/EmptyUtils.java
@@ -0,0 +1,51 @@
+package com.ddc.template.common.utils;
+
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * 判空工具类
+ * @author mumu.li
+ */
+public class EmptyUtils {
+
+    private EmptyUtils() {
+        throw new UnsupportedOperationException("");
+    }
+
+    /**
+     * 判断对象是否为空
+     * @param obj 对象
+     * @return true: 为空 false: 不为空
+     */
+    @SuppressWarnings("rawtypes")
+	public static boolean isEmpty(Object obj) {
+        if (obj == null) {
+            return true;
+        }
+        if (obj instanceof String && obj.toString().length() == 0) {
+            return true;
+        }
+        if (obj.getClass().isArray() && Array.getLength(obj) == 0) {
+            return true;
+        }
+        if (obj instanceof Collection && ((Collection) obj).isEmpty()) {
+            return true;
+        }
+        if (obj instanceof Map && ((Map) obj).isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 判断对象是否非空
+     * @param  obj 对象
+     * @return true: 非空  false: 空
+     */
+    public static boolean isNotEmpty(Object obj) {
+        return !isEmpty(obj);
+    }
+
+}
