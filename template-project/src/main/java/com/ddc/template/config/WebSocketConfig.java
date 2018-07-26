7e515175a10f1389500959f3335b26be72f8a28a
harry.zhang <harryzhang@daydaycook.com>
2018-07-26 14:03:22 +0800

    配置websockt导致不能用juit问题，先注释掉websockt 功能

--- template-project/src/main/java/com/ddc/template/config/WebSocketConfig.java
+++ template-project/src/main/java/com/ddc/template/config/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package com.ddc.template.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
@@ -11,7 +10,7 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 @Configuration
 public class WebSocketConfig {
 
-    @Bean
+//    @Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
     }
