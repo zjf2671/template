6a393591c9ad53b867a8e86f851102839352414f
harry.zhang <zjf2671@126.com>
2018-07-28 16:36:02 +0800

    on optimize the code

--- template-web/src/main/java/com/ddc/template/WebApplication.java
+++ template-web/src/main/java/com/ddc/template/WebApplication.java
@@ -1,12 +1,11 @@
 package com.ddc.template;
 
-import java.io.IOException;
-
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
+
+import java.io.IOException;
 
 @SpringBootApplication
 @ImportResource("classpath:config/consumer.xml")
